1c7e5e99825826c2cd0ffa59ce2cab2cd0576651
YinCheng0106 <eason010630@gmail.com>
2026-02-16 22:58:05 +0800

    feat: update baseline-browser-mapping to version 2.9.19 and add CircularText component for animated text display

--- src/app/(main)/link/page.tsx
+++ src/app/(main)/link/page.tsx
@@ -2,7 +2,10 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { motion } from "motion/react";
 import { notoSerifTC } from "@/app/fonts";
+
+import CircularText from "@/components/ui/circularText";
 
 export default function LinkPage() {
   const links = [
@@ -21,29 +24,50 @@ export default function LinkPage() {
 
   return (
     <div className="container flex flex-col items-center select-none">
-      <div className="my-8 flex w-full flex-col items-center gap-4">
-        <Image
-          src="/me/avatar.jpg"
-          alt="YinCheng"
-          width={120}
-          height={120}
-          className="rounded-full"
-        />
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        className="my-8 flex w-full flex-col items-center gap-4"
+      >
+        <div className="relative flex items-center justify-center">
+          <div className="pointer-events-none absolute inset-0 z-0 flex items-center justify-center">
+            <Image
+              src="/me/avatar.jpg"
+              alt="YinCheng"
+              width={120}
+              height={120}
+              className="rounded-full"
+            />
+          </div>
+          <CircularText
+            text="YinCheng • YinCheng • YinCheng • "
+            className={`text-primary ${notoSerifTC.className} z-20 h-[170px] w-[170px] font-bold`}
+            spinDuration={12}
+            onHover="slowDown"
+          />
+        </div>
         <h2 className={`text-4xl font-bold ${notoSerifTC.className}`}>
           YinCheng
         </h2>
-      </div>
+      </motion.div>
       <div className="flex w-full flex-col items-center">
         {links.map((link) => (
-          <div key={link.href} className="w-full">
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            key={link.href}
+            className="w-full"
+          >
             <Link href={link.href} target="_blank" rel="noopener noreferrer">
               <div
-                className={`bg-accent hover:bg-accent/80 my-2 rounded-xl px-6 py-3 text-center transition-transform duration-200 active:scale-95`}
+                className={`${notoSerifTC.className} bg-accent hover:bg-accent/80 my-2 transform rounded-full px-6 py-3 text-center font-semibold duration-200 ease-in-out active:scale-95`}
               >
                 {link.label}
               </div>
             </Link>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
